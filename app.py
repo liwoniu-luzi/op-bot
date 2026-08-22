596941e9f99b7aaae3a11a364b4074a79adf61bb
liwoniu-luzi <luzi1861a@gmail.com>
2026-08-22 18:32:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ NEZHA_PORT = os.environ.get('NEZHA_PORT', '')        # 哪吒v1请留空，哪�
 NEZHA_KEY = os.environ.get('NEZHA_KEY', '')          # 哪吒v0或v1密钥，哪吒面板后台命令里获取
 DOMAIN = os.environ.get('DOMAIN', 'op.lz1861.ccwu.cc')                # 项目分配的域名或反代后的域名,不包含https://前缀,例如: domain.xxx.com
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')         # 节点订阅token
-NAME = os.environ.get('NAME', '')                    # 节点名称
+NAME = os.environ.get('NAME', 'optiklink')                    # 节点名称
 WSPATH = os.environ.get('WSPATH', UUID[:8])          # 节点路径
 PORT = int(os.environ.get('SERVER_PORT') or os.environ.get('PORT') or 3000)  # http和ws端口，默认自动优先获取容器分配的端口
 AUTO_ACCESS = os.environ.get('AUTO_ACCESS', '').lower() == 'true' # 自动访问保活,默认关闭,true开启,false关闭,需同时填写DOMAIN变量
